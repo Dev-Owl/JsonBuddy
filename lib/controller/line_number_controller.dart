bbc135f5400e683f7a8d833373fadb1dc6b489f3
DevOwl <christian.muehle@devowl.de>
2022-05-01 20:41:41 +0200

    Cache for line numbers

--- lib/controller/line_number_controller.dart
+++ lib/controller/line_number_controller.dart
@@ -3,6 +3,8 @@ import 'package:flutter_highlight/theme_map.dart';
 import 'package:json_buddy/helper/global.dart';
 
 class LineNumberController extends TextEditingController {
+  TextSpan? _spanCache;
+  String _prevText = "";
   LineNumberController();
 
   @override
@@ -11,20 +13,25 @@ class LineNumberController extends TextEditingController {
     TextStyle? style,
     bool? withComposing,
   }) {
-    final themeToUse = prefs.getString(settingCodeTheme) ?? 'vs';
-    final children = <TextSpan>[];
-    final list = text.split('\n');
-    const themeClassToUse = 'root';
-    for (var k = 0; k < list.length; k++) {
-      final textSpan = TextSpan(
-        text: '${k + 1}',
+    if (_prevText != text) {
+      _prevText = text;
+      final themeToUse = prefs.getString(settingCodeTheme) ?? 'vs';
+      final children = <TextSpan>[];
+      final list = text.split('\n');
+      const themeClassToUse = 'root';
+      for (var k = 0; k < list.length; k++) {
+        final textSpan = TextSpan(
+          text: '${k + 1}',
+        );
+        children.add(textSpan);
+        if (k < list.length - 1) children.add(const TextSpan(text: '\n'));
+      }
+      _spanCache = TextSpan(
+        children: children,
+        style: themeMap[themeToUse]![themeClassToUse],
       );
-      children.add(textSpan);
-      if (k < list.length - 1) children.add(const TextSpan(text: '\n'));
     }
-    return TextSpan(
-      children: children,
-      style: themeMap[themeToUse]![themeClassToUse],
-    );
+
+    return _spanCache!;
   }
 }
